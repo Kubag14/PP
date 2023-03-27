68a46431addafda58e671523a9fc9060a15a9701
Kubag14 <kubagocc@gmail.com>
2023-03-27 13:07:05 +0200

    zajęcia 27.03

--- Program05/fbnA.c
+++ Program05/fbnA.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-void fbn_i(int n)
-{
-    for (n = ;c < n; c = a + b)
+int fbn_i(int n)
+{ unsigned i;
+    int a = 0, b = 1, c = 1;
+    for (i = 0; i < (n - 1); ++i) {
+        c = a + b;
+        a = b;
+        b = c;
+        }
         return c;
+}
+int main()
+{
+    unsigned n;
+    printf("Obliczanie ciagu Fibonacciego metoda iteracyjna\n");
+    printf("Podaj liczbe naturalna n: ");
+    scanf("%u",&n);
+    printf("%u\n",fbn_i(n));
 }
