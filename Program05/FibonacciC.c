68795759bd990613afd894f557d131c1eed3216a
Kubag14 <kubagocc@gmail.com>
2023-03-28 13:05:04 +0200

    28.03

--- Program05/FibonacciC.c
+++ Program05/FibonacciC.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 
-/* Obliczanie ciagu Fibonacciego - metoda iteracyjna */
+/* Obliczanie ciagu Fibonacciego - metoda iteracyjna z tablica */
 
-int fbn_r(int n)
-{
-    int k;
-    if (k < n)
-        return k+fbn_r(n+1);
-    else
-        return 1;
+void fbn_i(int fibonacci[], unsigned n)
+{ unsigned i;
+    fibonacci[0] = 0, fibonacci[1] = 1, fibonacci[2] = 1;// wartosci poczatkowe (rownoczesnie trzy pierwsze wyrazy ciagu)
+    for (i = 3; i < n; ++i) // petla powtarza sie, poki i jest mniejsze od n - 1, gdzie n jest numerem liczby w ciagu
+    {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]; // Fn = F(n-1) + F(n-2)
+    }
 }
-
 int main()
 {
-    int n;
-    printf("Obliczanie ciagu Fibonacciego metoda iteracyjna\n");
-    printf("Podaj liczbe naturalna n: ");
-    scanf("%u",&n);
-    printf("%u\n",fbn_r(n)); // wypisanie wyniku funkcji - odpowiedniej liczby Fn
+    unsigned tmin,tmax;
+    unsigned t;
+    printf("Obliczanie ciagu Fibonacciego - metoda iteracyjna z tablica.\n");
+    do {
+        printf("Podaj rozmiar tablicy - liczbe naturalna t = ");
+        scanf("%u", &t); // wczytanie rozmiaru tablicy t
+    } while (t < 1);
+
+    int fibonacci[t]; // definiujemy tablice o rozmiarze t
+    fbn_i(fibonacci,t); // wywolanie funkcji, ktora wypelni tablice fibonacci o rozmiarze t
+    printf("Rozmiar tablicy t = %u\n",t);
+
+    do {
+        printf("Podaj dwie liczby naturalne w formacie n, k (od, do; k>=n, 0<=k<=t): "); // wczytanie tmin i tmax
+        scanf("%u,%u", &tmin, &tmax); //
+    } while (tmax < tmin || tmax > t || tmin < 0); // jesli tmax < tmin, tmax > t lub tmin < 0 to powtarzamy petle
+   unsigned k = tmin;
+   for (tmin >= 0 || tmax >= 0; k <= tmax; ++k)
+        printf("%u\n", fibonacci[k]); // wypisanie wartosci z tablicy z podanego zakresu
 }
